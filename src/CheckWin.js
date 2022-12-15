a0cb5f01b7057529ad1e3e050be1188177b34e77
shoham sofer <92480018+sofer1445@users.noreply.github.com>
2022-12-15 13:59:18 +0200

    shoham ,13:59

--- src/CheckWin.js
+++ src/CheckWin.js
@@ -1,30 +1,39 @@
 function checkWin(props) {
     console.log("checkWin" + ", " + props);
     const oneDimensionalArray = props.slice();
-    console.log("squares: " + oneDimensionalArray);
-    const twoDimensionalArray = oneDimensionalArray.map((element) => [element]);
+    console.log("squares: " + oneDimensionalArray + "," + oneDimensionalArray.length);
+    // const twoDimensionalArray = oneDimensionalArray.map((element) => [element]);
 
     for (let row = 0; row < oneDimensionalArray.length / 6; row++) {
         // Loop through each column on the board
         for (let col = 0; col < oneDimensionalArray.length / 7; col++) {
             // Check if there is a winner
-            console.log(twoDimensionalArray);
-            console.log("row: " + row + " col: " + col + " , " + twoDimensionalArray[row][col]);
+            console.log(oneDimensionalArray);
+            console.log("row: " + row + " col: " + col + " , " + oneDimensionalArray[row][col]);
             try {
-                if (twoDimensionalArray[row][col] === twoDimensionalArray[row][col + 1] && twoDimensionalArray[row][col] === twoDimensionalArray[row][col + 2] && twoDimensionalArray[row][col] === twoDimensionalArray[row][col + 3]) {
-                    console.log("win");
+
+                if (oneDimensionalArray[row][col] === oneDimensionalArray[row][col + 1]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row][col + 2]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row][col + 3]) {
+                    console.log("win" + "תנאי ראשון");
                     return true;
                 }
-                if (twoDimensionalArray[row][col] === twoDimensionalArray[row + 1][col] && twoDimensionalArray[row][col] === twoDimensionalArray[row + 2][col] && twoDimensionalArray[row][col] === twoDimensionalArray[row + 3][col]) {
-                    console.log("win");
+                if (oneDimensionalArray[row][col] === oneDimensionalArray[row + 1][col]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row + 2][col]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row + 3][col]) {
+                    console.log("win" + "תנאי שני");
                     return true;
                 }
-                if (twoDimensionalArray[row][col] === twoDimensionalArray[row + 1][col + 1] && twoDimensionalArray[row][col] === twoDimensionalArray[row + 2][col + 2] && twoDimensionalArray[row][col] === twoDimensionalArray[row + 3][col + 3]) {
-                    console.log("win");
+                if (oneDimensionalArray[row][col] === oneDimensionalArray[row + 1][col + 1]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row + 2][col + 2]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row + 3][col + 3]) {
+                    console.log("win" + "תנאי שלישי");
                     return true;
                 }
-                if (twoDimensionalArray[row][col] === twoDimensionalArray[row - 1][col + 1] && twoDimensionalArray[row][col] === twoDimensionalArray[row - 2][col + 2] && twoDimensionalArray[row][col] === twoDimensionalArray[row - 3][col + 3]) {
-                    console.log("win");
+                if (oneDimensionalArray[row][col] === oneDimensionalArray[row - 1][col + 1]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row - 2][col + 2]
+                    && oneDimensionalArray[row][col] === oneDimensionalArray[row - 3][col + 3]) {
+                    console.log("win" + "תנאי רביעי");
                     return true;
                 }
             } catch (e) {
